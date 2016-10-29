2fa6e486ed719a01acbb7fcc1eb60dd2933ab2c8
Sprax Lines <sprax@users.noreply.github.com>
2016-10-29 12:52:09 -0400

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="demo">fun()</p>
+
+<script>
+function fun() { return document.getElementById("demo").innerHTML = "Hello World!"; }
+fun()
+</script>
+
+</body>
+</html>
